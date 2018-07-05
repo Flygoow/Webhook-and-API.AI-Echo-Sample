0556edffc45802801cb861ac57c787e0aef2485e
Jérémy <30751347+Flygoow@users.noreply.github.com>
2018-07-05 23:42:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ restService.post("/audio", function(req, res) {
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "webhook-echo-sample"
+    source: "cod-forlan-web"
   });
 });
 
@@ -126,7 +126,7 @@ restService.post("/video", function(req, res) {
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     displayText:
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
+    source: "cod-forlan-web"
   });
 });
 
